edbb581f899261dc8d918b1787bd101f5a1e4470
admin-PC\admin <123@qq.com>
2018-12-27 10:18:48 +0800

    删除数据库

--- src/main/resources/db/migration/V2_1__Drop_TalkbackNumber_from_TalbackUser.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `t_talkback_user` DROP FOREIGN KEY `FKh3qcqg506lwmjlanrcytrscm6`;
-
-ALTER TABLE `t_talkback_user` DROP COLUMN `talback_number_id`;
-
--- src/main/resources/db/migration/V2_3__Remove_TalkbackUser_from_TalkbackNumber.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `t_talkback_number` DROP FOREIGN KEY `FKjk68bf80efqsjp3fhumn9rnn7`;
-
-ALTER TABLE `t_talkback_number`
-DROP COLUMN `talkback_user_id`;
